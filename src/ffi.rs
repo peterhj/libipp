5a8f7f90a8f2b3ff2f832dd8ffd2cf800b141920
Peter Jin <peterhaijin@gmail.com>
2017-01-28 11:28:13 -0800

    Initial commit. Currently only supports linear resizing of byte-valued, 1-channel image planes.

---
+++ src/ffi.rs
@@ -0,0 +1,84 @@
+#![allow(non_camel_case_types)]
+
+use libc::*;
+
+//pub struct IppStatus(pub c_int);
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum IppStatus {
+  IppStsNoErr = 0,
+  // TODO
+}
+
+impl IppStatus {
+  pub fn is_ok(self) -> bool {
+    self as i32 == 0
+  }
+
+  pub fn is_err(self) -> bool {
+    !self.is_ok()
+  }
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub enum IppiInterpolationType {
+  ippNearest    = 1,
+  ippLinear     = 2,
+  ippCubic      = 6,
+  ippLanczos    = 16,
+  ippHahn       = 0,
+  ippSuper      = 8,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct IppiRect {
+  pub x:        c_int,
+  pub y:        c_int,
+  pub width:    c_int,
+  pub height:   c_int,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct IppiPoint {
+  pub x:        c_int,
+  pub y:        c_int,
+}
+
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct IppiSize {
+  pub width:    c_int,
+  pub height:   c_int,
+}
+
+/*#[repr(C)]
+pub enum IppiResizeFilterType {
+}
+
+pub enum IppiResizeFilterState {}*/
+
+pub type IppiResizeSpec_32f = u8;
+
+#[link(name = "ipps")]
+extern "C" {
+  pub fn ippsMalloc_8u(size: c_int) -> *mut u8;
+  pub fn ippsFree(ptr: *mut c_void);
+}
+
+#[link(name = "ippi")]
+extern "C" {
+  pub fn ippiMalloc_8u_C1(width_pixels: c_int, height_pixels: c_int, pitch: *mut c_int) -> *mut u8;
+  pub fn ippiFree(ptr: *mut c_void);
+
+  pub fn ippiCopy_8u_C1R(src: *const u8, src_pitch: c_int, dst: *mut u8, dst_pitch: c_int, roi_size: IppiSize) -> IppStatus;
+
+  pub fn ippiResizeGetSize_8u(src_size: IppiSize, dst_size: IppiSize, interpolation: IppiInterpolationType, antialiasing: u32, spec_size: *mut c_int, init_buf_size: *mut c_int) -> IppStatus;
+  pub fn ippiResizeGetBufferSize_8u(spec: *const IppiResizeSpec_32f, dst_size: IppiSize, num_channels: u32, buf_size: *mut c_int) -> IppStatus;
+  //pub fn ippiResizeGetBorderSize_8u(spec: *const IppiResizeSpec_32f, border_size: *mut IppiBorderSize) -> IppStatus;
+  pub fn ippiResizeLinearInit_8u(src_size: IppiSize, dst_size: IppiSize, spec: *mut IppiResizeSpec_32f) -> IppStatus;
+  pub fn ippiResizeLinear_8u_C1R(src: *const u8, src_pitch: i32, dst: *mut u8, dst_pitch: i32, dst_offset: IppiPoint, dst_size: IppiSize, spec: *const IppiResizeSpec_32f, buf: *mut u8) -> IppStatus;
+}
---
+++ src/lib.rs
@@ -0,0 +1,209 @@
+extern crate icc_link;
+extern crate libc;
+
+use ffi::*;
+
+use std::marker::{PhantomData};
+//use std::ops::{Deref, DerefMut};
+
+pub mod ffi;
+
+pub struct IppTemporalBuf<T> where T: Copy {
+  ptr:      *mut T,
+  _size:    usize,
+}
+
+impl<T> Drop for IppTemporalBuf<T> where T: Copy {
+  fn drop(&mut self) {
+    assert!(!self.ptr.is_null());
+    unsafe { ippsFree(self.ptr as *mut _) };
+  }
+}
+
+impl IppTemporalBuf<u8> {
+  pub fn alloc(size: usize) -> IppTemporalBuf<u8> {
+    let ptr = unsafe { ippsMalloc_8u(size as _) };
+    assert!(!ptr.is_null());
+    IppTemporalBuf{
+      ptr:      ptr,
+      _size:    size,
+    }
+  }
+
+  pub fn as_ptr(&self) -> *const u8 {
+    self.ptr
+  }
+
+  pub fn as_mut_ptr(&mut self) -> *mut u8 {
+    self.ptr
+  }
+}
+
+pub struct IppImageBuf<T> where T: Copy {
+  ptr:      *mut T,
+  width:    usize,
+  height:   usize,
+  pitch:    usize,
+}
+
+impl<T> Drop for IppImageBuf<T> where T: Copy {
+  fn drop(&mut self) {
+    assert!(!self.ptr.is_null());
+    unsafe { ippiFree(self.ptr as *mut _) };
+  }
+}
+
+impl IppImageBuf<u8> {
+  pub fn alloc(width: usize, height: usize) -> IppImageBuf<u8> {
+    let mut pitch: i32 = 0;
+    let ptr = unsafe { ippiMalloc_8u_C1(width as _, height as _, &mut pitch as *mut _) };
+    assert!(!ptr.is_null());
+    IppImageBuf{
+      ptr:      ptr,
+      width:    width,
+      height:   height,
+      pitch:    pitch as _,
+    }
+  }
+
+  pub fn load(&mut self, ext_buf: &[u8]) {
+    assert_eq!(ext_buf.len(), self.width * self.height);
+    let status = unsafe { ippiCopy_8u_C1R(
+        ext_buf.as_ptr(),
+        self.width as _,
+        self.ptr,
+        self.pitch as _,
+        IppiSize{width: self.width as _, height: self.height as _},
+    ) };
+    assert!(status.is_ok());
+  }
+
+  pub fn store(&self, ext_buf: &mut [u8]) {
+    assert_eq!(ext_buf.len(), self.width * self.height);
+    let status = unsafe { ippiCopy_8u_C1R(
+        self.ptr,
+        self.pitch as _,
+        ext_buf.as_mut_ptr(),
+        self.width as _,
+        IppiSize{width: self.width as _, height: self.height as _},
+    ) };
+    assert!(status.is_ok());
+  }
+}
+
+pub struct IppImageResizeLinear<T> where T: Copy {
+  spec: IppTemporalBuf<u8>,
+  buf:  IppTemporalBuf<u8>,
+  src:  (usize, usize),
+  dst:  (usize, usize),
+  _mrk: PhantomData<fn (T)>,
+}
+
+impl IppImageResizeLinear<u8> {
+  pub fn new(src_width: usize, src_height: usize, dst_width: usize, dst_height: usize) -> Self {
+    let mut spec_size = 0;
+    let mut init_buf_size = 0;
+    let status = unsafe { ippiResizeGetSize_8u(
+        IppiSize{width: src_width as _, height: src_height as _},
+        IppiSize{width: dst_width as _, height: dst_height as _},
+        IppiInterpolationType::ippLinear,
+        0, // antialiasing.
+        &mut spec_size as *mut _,
+        &mut init_buf_size as *mut _,
+    ) };
+    assert!(status.is_ok());
+    assert_eq!(0, init_buf_size);
+    let mut spec = IppTemporalBuf::<u8>::alloc(spec_size as _);
+    let status = unsafe { ippiResizeLinearInit_8u(
+        IppiSize{width: src_width as _, height: src_height as _},
+        IppiSize{width: dst_width as _, height: dst_height as _},
+        spec.as_mut_ptr() as *mut _,
+    ) };
+    assert!(status.is_ok());
+    let mut buf_size = 0;
+    let status = unsafe { ippiResizeGetBufferSize_8u(
+        spec.as_ptr() as *const IppiResizeSpec_32f,
+        IppiSize{width: dst_width as _, height: dst_height as _},
+        1, // num channels.
+        &mut buf_size as *mut _,
+    ) };
+    assert!(status.is_ok());
+    let buf = IppTemporalBuf::<u8>::alloc(buf_size as _);
+    IppImageResizeLinear{
+      spec: spec,
+      buf:  buf,
+      src:  (src_width, src_height),
+      dst:  (dst_width, dst_height),
+      _mrk: PhantomData,
+    }
+  }
+
+  pub fn resize(&mut self, src: &IppImageBuf<u8>, dst: &mut IppImageBuf<u8>) {
+    assert_eq!(self.src.0, src.width);
+    assert_eq!(self.src.1, src.height);
+    assert_eq!(self.dst.0, dst.width);
+    assert_eq!(self.dst.1, dst.height);
+    let status = unsafe { ippiResizeLinear_8u_C1R(
+        src.ptr,
+        src.pitch as _,
+        dst.ptr,
+        dst.pitch as _,
+        IppiPoint{x: 0, y: 0},
+        IppiSize{width: dst.width as _, height: dst.height as _},
+        self.spec.as_ptr() as *const IppiResizeSpec_32f,
+        self.buf.as_mut_ptr(),
+    ) };
+    assert!(status.is_ok());
+  }
+}
+
+pub struct IppImageDownsamplePyramid<T> where T: Copy {
+  bufs: Vec<IppImageBuf<T>>,
+  ops:  Vec<IppImageResizeLinear<T>>,
+  src:  (usize, usize),
+  dst:  (usize, usize),
+}
+
+impl IppImageDownsamplePyramid<u8> {
+  pub fn new(src_width: usize, src_height: usize, dst_width: usize, dst_height: usize) -> Self {
+    let mut bufs = vec![];
+    let mut ops = vec![];
+    bufs.push(IppImageBuf::<u8>::alloc(src_width, src_height));
+    let mut prev_width = src_width;
+    let mut prev_height = src_height;
+    while prev_width > dst_width && prev_height > dst_height {
+      let next_width = if prev_width >= 2 * dst_width {
+        (prev_width + 1) / 2
+      } else {
+        dst_width
+      };
+      let next_height = if prev_height >= 2 * dst_height {
+        (prev_height + 1) / 2
+      } else {
+        dst_height
+      };
+      bufs.push(IppImageBuf::<u8>::alloc(next_width, next_height));
+      ops.push(IppImageResizeLinear::<u8>::new(prev_width, prev_height, next_width, next_height));
+      prev_width = next_width;
+      prev_height = next_height;
+    }
+    IppImageDownsamplePyramid{
+      bufs: bufs,
+      ops:  ops,
+      src:  (src_width, src_height),
+      dst:  (dst_width, dst_height),
+    }
+  }
+
+  pub fn downsample(&mut self, src: &[u8], dst: &mut [u8]) {
+    assert_eq!(self.src.0 * self.src.1, src.len());
+    assert_eq!(self.dst.0 * self.dst.1, dst.len());
+    let num_levels = self.ops.len();
+    self.bufs[0].load(src);
+    for k in 0 .. num_levels {
+      let (prev_bufs, mut next_bufs) = self.bufs.split_at_mut(k+1);
+      self.ops[k].resize(&prev_bufs[k], &mut next_bufs[0]);
+    }
+    self.bufs[num_levels].store(dst);
+  }
+}
